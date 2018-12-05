52ba4f4edbe941337219c7903a8c1591d51c6e9d
pablo-brusset <pablo.brusset34@gmail.com>
2018-12-05 10:45:01 +0100

    mon premier commit

---
+++ usermultication.java
@@ -0,0 +1,22 @@
+package exercice;
+
+import java.util.Scanner;
+
+class usermultication {
+	public static void main(String[] args)
+	{
+		int saisieutilisateur,i; 
+		System.out.println("Enter your table choice");
+		
+		Scanner in=new Scanner(System.in);
+		saisieutilisateur=in.nextInt();
+		
+		System.out.println("Multiplication table of "+saisieutilisateur);
+		
+		for(i=1;i<=10;i++) //Permet de signaler a mon opérateur de calculer que jusqu'à 10              		
+		{
+			System.out.println(saisieutilisateur+"*"+i+"="+(saisieutilisateur*i));
+			in.close(); // Permet de fermer la saisie 
+		}
+	}
+}
